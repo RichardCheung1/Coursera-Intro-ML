df2cd64ca728fda8e2c73603fb555453447cf7ef
Richard Cheung <Richard Cheung>
2017-03-02 22:59:34 -0500

    completed ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,19 +41,17 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+J = (1/2) * sum(sum(((X*Theta' - Y) .* R).^2));
+X_grad = (X*Theta' - Y).* R * Theta;
+Theta_grad = ((X*Theta' - Y ).*R)' * X;
 
+%REGULARIZED
+Theta_regularization = lambda/2 .* sum(sum(Theta.^2));
+X_regularization = lambda/2 .*sum(sum(X.^2));	
+J = J + Theta_regularization + X_regularization;
 
-
-
-
-
-
-
-
-
-
-
-
+X_grad = X_grad + lambda * X; 
+Theta_grad = Theta_grad + lambda * Theta; 
 
 % =============================================================
 
